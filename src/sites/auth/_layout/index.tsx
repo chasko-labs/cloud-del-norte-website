5c72929a61eec396505af8b7cf2aa442b087589c
Bryan Chasko <bryanchasko@gmail.com>
2026-05-05 22:00:26 +0000

    chore: bump auth version to 0.0.0002

--- src/sites/auth/_layout/index.tsx
+++ src/sites/auth/_layout/index.tsx
@@ -78,7 +78,7 @@ export default function AuthLayout({
 				</div>
 				<div className="cdn-card cdn-auth-card cdn-glass">
 					<div className="cdn-auth-form">{children}</div>
-					<span className="cdn-auth-version">auth 0.0.0001</span>
+					<span className="cdn-auth-version">auth 0.0.0002</span>
 				</div>
 			</ContentLayout>
 		</Shell>
