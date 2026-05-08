4e3c06b92d2d6a4aa6d46fe0de87e05fd2051aad
Bryan Chasko <reddythafox@hotmail.com>
2026-05-07 22:06:52 -0600

    chore: bump version to 0.0.0132

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -21,7 +21,7 @@ export default function Footer() {
 					<strong className="cdn-footer-emphasis">{t("footer.goBuild")}</strong>
 					.
 				</p>
-				<span className="cdn-version">0.0.0123</span>
+				<span className="cdn-version">0.0.0132</span>
 			</div>
 		</footer>
 	);
